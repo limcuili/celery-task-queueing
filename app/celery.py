220eba26c1d67da59c85afb11d23258859907c04
limcuili <limcuili@gmail.com>
2021-07-04 18:18:12 +0100

    replace task with shared_task and execute group tasks

--- app/celery.py
+++ app/celery.py
@@ -9,7 +9,7 @@ app.config_from_object('app.celeryconfig')
 
 app.conf.beat_schedule = {
     "everyday-task": {
-        "task": "app.tasks.hello_world",
+        "task": "app.tasks.group_add",
         "schedule": crontab(minute="*")
     }
 }
